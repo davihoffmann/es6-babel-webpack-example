73497a5ac4b247bdfff0674afa871a6ccdebd519
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-05-12 19:47:41 -0300

    feito busca de repositorio da api do github e mostrado em tela

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import api from './api';
+
 class App {
 
     constructor() {
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
         this.listEl = document.getElementById('repo-list');
 
         this.registerHandlers();
@@ -13,15 +16,26 @@ class App {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
-    addRepository(event) {
+    async addRepository(event) {
         event.preventDefault();
 
+        const repoInput = this.inputEl.value;
+
+        if(repoInput.length === 0)
+            return;
+
+        const response = await api.get(`/repos/${repoInput}`);
+
+        const { name, description, html_url, owner: { avatar_url } } = response.data;
+
         this.repositories.push({
-            name: 'es6-babel-webpack-example',
-            description: 'Projeto de exemplo de um projeto es com babel e webpack',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-            html_url: 'https://github.com/davihoffmann/es6-babel-webpack-example',
+            name,
+            description,
+            avatar_url,
+            html_url,
         });
+
+        this.inputEl.value = '';
 
         this.render();
     }
@@ -41,6 +55,7 @@ class App {
 
             let linkEl = document.createElement('a');
             linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
             linkEl.appendChild(document.createTextNode('Acessar'));
 
             let listItemEl = document.createElement('li');
